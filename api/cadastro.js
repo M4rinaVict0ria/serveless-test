bd31589891647aecb4601cd615346ded4203916b
Marina Freitas <160733141+M4rinaVict0ria@users.noreply.github.com>
2025-08-20 13:53:37 +0000

    test

---
+++ api/cadastro.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+    if (req.method === "POST") {
+        const { nome, email } = req.body;
+        res.status(200).json({ mensagem: `Cadastro realizado com sucesso para ${nome} (${email})` });
+    } else {
+        res.status(405).json({ mensagem: "Método não permitido" });
+    }
+}
---
+++ api/hello.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+    const nome = req.query.nome || "mundo";
+    res.status(200).json({ mensagem: `OLá, ${nome}!`});
+    
+}
