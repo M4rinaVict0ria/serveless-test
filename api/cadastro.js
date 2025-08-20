ec4396b091963e8dadfd744cec29739830b8ca7a
Marina Freitas <160733141+M4rinaVict0ria@users.noreply.github.com>
2025-08-20 14:02:12 +0000

    test 6

--- api/cadastro.js
+++ api/cadastro.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     if (req.method === "POST") {
         const { nome, email } = req.body;
-        res.status(200).json({ mensagem: `Cadastro realizado com sucesso para ${nome} (${email})` });
+        res.status(200).json({ mensagem: `Cadastro realizado com sucesso para` });
     } else {
         res.status(405).json({ mensagem: "Método não permitido" });
     }
